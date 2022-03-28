d519330063ef2504d8581ea5c441772843f61c86
Elijah Chou <53510948+elijah-chou@users.noreply.github.com>
2022-03-27 22:56:27 -0400

    updata jdbc demo for GCP MySQL database

--- JBDC_GCP_DEMO/MyJDBC.java
+++ JBDC_GCP_DEMO/MyJDBC.java
@@ -1,22 +1,24 @@
 
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MyJDBC {
-    public static void main(String[] args) {
+    public static final String CREDENTIALS_STRING = "jdbc:mysql://google/test?cloudSqlInstance=cs370-x:us-east1:elijahchou&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=test&password=password";
+    static Connection connection = null;
 
+    public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://34.132.28.43:3306/kwai", "matthew", "Kw@1v1su@l1z3r");
+
+            Connection connection = DriverManager.getConnection(CREDENTIALS_STRING);
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from user");
+            ResultSet resultSet = statement.executeQuery("select * from testTable");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("firstname"));
+                System.out.println(resultSet.getString("guestName"));
             }
         } catch(Exception e) {
             e.printStackTrace();
